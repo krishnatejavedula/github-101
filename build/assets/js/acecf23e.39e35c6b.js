668a53b6d1cb9923a551c756868ba065342b17b8
Krishna Teja Vedula <vedula@mtu.edu>
2025-07-19 20:30:34 -0400

    Removed build files and updated gitignore

--- build/assets/js/acecf23e.39e35c6b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/github-101/blog","blogTitle":"Blog","authorsListPath":"/github-101/blog/authors"}')}}]);
